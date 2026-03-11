34f5f8d53d33a8e648e9e9e0ec171bf38f864b3c
Chen-hailu <63396970+Chen-hailu@users.noreply.github.com>
2026-03-11 19:00:42 +0900

    Add files via upload

---
+++ main.m
@@ -0,0 +1,92 @@
+clc
+clear
+
+%% If you use our toolbox or if you find our research helpful, please cite the following paper (thanks for your support):
+%% Chen HL, Shen YZ. and Zhang L. 2025. Iterative Weighted Least Squares Solution to InSAR Stratified Tropospheric Phase Delay. IEEE Journal of Selected Toptics in Applied Earth Observations and Remote Sensing, 18, 19594 - 19605
+%% Any feedback is welcome! (2210929@tongji.edu.cn or yzshen@tongji.edu.cn)
+
+%% 1. Prepare input data
+addpath '.\weighted_InSAR strified_delay_estimation'
+ts_file='timeseries_SET.h5';% Sentinel time series derived from MintPy and ISCE2
+s=3;% Descending take as 3; Ascending take as 1
+[phase,longitude1,latitude1,height1]= read_H5data(ts_file,s);
+save InSAR_input.mat phase phase longitude1 latitude1 height1
+
+%% 2. Predefined parameters
+ Number_segments=40;%Used for calculating covariance
+ sample=10;% Sparse sampling, reducing the computational burden
+
+%% 3. Set reference point
+refpoint=[1200,520];% The reference point can be chosen arbitrarily. The results under different reference points vary very little which is also the advantage of this weighted method.
+height1=height1-height1(refpoint(1,1),refpoint(1,2));
+phase=phase-phase(refpoint(1,1),refpoint(1,2),:);
+imagesc(height1);colorbar;colormap(jet)
+
+%% 4. DEM normalization
+height1=height1./(max(height1(:))-min(height1(:)));
+
+%% 5. Set up the function model for estimating stratification
+order=2; %  1:first-order linear;2::second-order linear(When there is a large height difference, adopt)
+
+%% 6. Establish a differential time series as the observed values to weaken the influence of deformation.
+for i=1:size(phase,3)-1
+phase_diff(:,:,i)=phase(:,:,i+1)-phase(:,:,i);
+end
+clear phase
+
+%% 7. Data downsampling
+latitude1_sample = latitude1(1:sample:end, 1:sample:end);
+longitude1_sample = longitude1(1:sample:end, 1:sample:end);
+phase_diff_sample = phase_diff(1:sample:end, 1:sample:end,:);
+height1_sample = height1(1:sample:end, 1:sample:end);
+clear latitude1 longitude1 
+location=[longitude1_sample(:) latitude1_sample(:)];
+location(any(isnan(location),2),:) = [];
+[X,Y]= Pub_BLH2XYZ(location(:,2),location(:,1));
+ XY=[X Y];
+ clear Y X Y1 X1
+ DS=squareform(pdist(XY));
+ DS=single(DS);
+  
+clear location longitude1_sample latitude1_sample
+%% 8. Estimate the parameters of the linear model
+%for i=2:size(phase_diff_sample,3)-190
+for i=2
+     l=phase_diff_sample(:,:,i);
+     [par_weight(i,:),par_unweight(i,:)]=Par_estimation(l,height1_sample,DS,Number_segments,order);
+     i
+ end
+
+%% 9. time series correction
+figure
+imagesc(phase_diff(:,:,2));colorbar;caxis([-5 5])
+hold on
+figure
+imagesc(phase_diff(:,:,2)-height1.*0.83600432);colorbar;caxis([-5 5])
+
+
+
+dem=height1(:);
+dem(any(isnan(dem),2),:) = [];
+if order==1
+     hgt=dem;
+end
+if order==2
+     hgt=[dem dem.*dem];
+end
+
+Str_correction=zeros(size(phase_diff_sample,1),size(phase_diff_sample,2),size(phase_diff_sample,3));
+Str_correction(:,:,1)=0;
+for i=2:size(phase_diff_sample,3)
+  Str_correction(:,:,i)=Str_correction(:,:,i-1)+ hgt
+
+end
+
+
+
+%% 10. Outlooks
+% For situations where stratification needs to be estimated, after the window has been divided, the model coefficients can be estimated using weighted methods.
+% The remaining severe turbulence has not been dealt with yet. We will update the code for handling turbulence in the future.
+
+
+
